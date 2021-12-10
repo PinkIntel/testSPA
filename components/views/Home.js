94469d2249f0077b1f2f62c3b2a4b5cb4b496589
Julliette Huynh <isakurakitsune@gmail.com>
2021-12-10 11:12:54 -0600

    Test SPA from Class

---
+++ components/views/Home.js
@@ -0,0 +1,14 @@
+import html from "html-literal";
+import catImage from '/assets/images/cat.jpg';
+
+export default () => html`
+  <section id="jumbotron">
+    <h2>Savvy Coders Apr. 2020 Cohort</h2>
+    <a href="" onClick="alert('Hello! You clicked the button!')"
+      >"Call to Action" "Button"</a
+    >
+  </section>
+  <img src="${catImage}" />
+`;
+
+
